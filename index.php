827cb5ee6d620677f1bbc3b7c98923b45216da76
Tiago Daniel <187806573+tiagodan777@users.noreply.github.com>
2026-04-23 12:27:18 +0000

    imagnes e codigo inicial

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+require_once 'assets.php';
+
+
+?>
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <main>
+        <article>
+            <header>
+                <h2></h2>
+            </header>
+            <section>
+                <img src="" alt="foto-teste">
+            </section>
+            <aside>
+                <p></p>
+            </aside>
+        </article>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
